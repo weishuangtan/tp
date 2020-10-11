ab21fab0aece39cf5f02f06bd84cb5c4277483db
tengkianen <tengkianen@gmail.com>
2020-10-11 19:30:41 +0800

    no message

--- src/main/java/seedu/trippie/command/ListPlacesCommand.java
+++ src/main/java/seedu/trippie/command/ListPlacesCommand.java
@@ -1,23 +1,35 @@
 package seedu.trippie.command;
 
+<<<<<<< HEAD
 import seedu.trippie.Expense;
 import seedu.trippie.ExpenseList;
 import seedu.trippie.PlaceList;
 import seedu.trippie.Ui;
 
 public class ListPlacesCommand extends Command{
+=======
+public class ListPlacesCommand extends Command {
+>>>>>>> Update List command
 
     public ListPlacesCommand() {
 
     }
 
+<<<<<<< HEAD
     @Override
+=======
+
+>>>>>>> Update List command
     public boolean isExit() {
         return false;
     }
 
+<<<<<<< HEAD
     @Override
     public void execute(Ui ui, PlaceList placeList, ExpenseList expense) {
 
     }
+=======
+    public void execute(){};
+>>>>>>> Update List command
 }
