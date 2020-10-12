b46a6cf8138ddce5ac3cccfaca705e2b6c36b5ce
tengkianen <tengkianen@gmail.com>
2020-10-12 23:52:28 +0800

    Test I/O, Fix bugs

--- src/main/java/seedu/trippie/Parser.java
+++ src/main/java/seedu/trippie/Parser.java
@@ -27,7 +27,7 @@ public class Parser {
                 return new DeleteExpenditureCommand(userInput);
             } else if (userInput.equals("list /e")) {
                 return new ListExpenseCommand();
-            } else if (userInput.startsWith("budget")) {
+            } else if (userInput.startsWith("budget ")) {
                 return new BudgetCommand(userInput);
             } else if (userInput.equals("spending")) {
                 return new DisplayTotalExpenditureCommand();
@@ -39,7 +39,7 @@ public class Parser {
                 return new HelpCommand();
             } else if (userInput.startsWith("list /p")) {
                 return new ListPlacesCommand(userInput);
-            } else if (userInput.startsWith("search")) {
+            } else if (userInput.startsWith("search ")) {
                 return new SearchCommand(userInput);
             } else {
                 System.out.println("Invalid Command! Type \"help\" to view the list of available commands!");
