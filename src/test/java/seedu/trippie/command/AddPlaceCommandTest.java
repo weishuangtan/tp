f7c68df80594f26031b6aaf8938c15c12ea164bd
felixhalim <felixh@comp.nus.edu.sg>
2020-10-21 22:54:57 +0800

    Fix Style to adhere with Gradle

--- src/test/java/seedu/trippie/command/AddPlaceCommandTest.java
+++ src/test/java/seedu/trippie/command/AddPlaceCommandTest.java
@@ -9,17 +9,21 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AddPlaceCommandTest {
 
-    private final String[] goodUserInputs = {"add /n Marina Bay Sands /d 2 /t 1000 to 1200",
-            "add /n Changi Airport /d 1 /t 1900 to 2000",
-            "add /n Bugis Junction /d 3 /t 1100 to 1400"};
+    private final String[] goodUserInputs = {
+        "add /n Marina Bay Sands /d 2 /t 1000 to 1200",
+        "add /n Changi Airport /d 1 /t 1900 to 2000",
+        "add /n Bugis Junction /d 3 /t 1100 to 1400"
+    };
 
-    private final String[] badUserInputs = {"add /n Marina Bay Sands /d 2 /t 1300 to 1200",
-            "add /n Changi Airport /d one /t 1900 to 2000",
-            "add /n Bugis /d 1 /t 2100 to 2000",
-            "add /n Clementi /d 4 /t 9999 to 0100",
-            "addx /n Haw Par Villa /d 3 /t 0100 to 0500",
-            "add /n Apple Park /d 3 /t 1300 1200",
-            "add /n"};
+    private final String[] badUserInputs = {
+        "add /n Marina Bay Sands /d 2 /t 1300 to 1200",
+        "add /n Changi Airport /d one /t 1900 to 2000",
+        "add /n Bugis /d 1 /t 2100 to 2000",
+        "add /n Clementi /d 4 /t 9999 to 0100",
+        "addx /n Haw Par Villa /d 3 /t 0100 to 0500",
+        "add /n Apple Park /d 3 /t 1300 1200",
+        "add /n"
+    };
 
     @Test
     void addPlaceCommand_validUserInput_setPlaceParameters() throws TrippieInvalidArgumentException {
