4ed6cbbcc1dd0fe7ffd9b143e0cced95a2c1f346
Ivander <ivanderjmw@gmail.com>
2020-10-21 12:25:51 +0800

    Fix code style in StorageTest

--- src/main/java/seedu/trippie/command/NewTripCommand.java
+++ src/main/java/seedu/trippie/command/NewTripCommand.java
@@ -19,27 +19,31 @@ public class NewTripCommand extends Command {
 
     public void execute(Ui ui, TrippieData trippieData) {
         System.out.print("Enter your new trip's name:");
-        String name = ui.getLine();
+        final String name = ui.getLine();
 
         System.out.print("Enter your new trip's start date (dd-mm-yyyy):");
         SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
-        Date startDate = null;
+        final Date startDate;
+        Date tempStartDate = null;
         try {
-            startDate = df.parse(ui.getLine());
+            tempStartDate = df.parse(ui.getLine());
         } catch (ParseException e) {
             e.printStackTrace();
+            tempStartDate = null;
         }
+
+        startDate = tempStartDate;
 
         int index = trippieData.getTripList().size();
 
         System.out.print("Enter the foreign exchange rate:");
-        Float forEx = Float.parseFloat(ui.getLine());
+        final Float forEx = Float.parseFloat(ui.getLine());
 
         System.out.print("Enter the foreign currency abbreviation (eg. MYR): ");
-        String currencyAbbreviation = ui.getLine();
+        final String currencyAbbreviation = ui.getLine();
 
         System.out.print("Enter your budget for the trip (in SGD):");
-        Float budget = Float.parseFloat(ui.getLine());
+        final Float budget = Float.parseFloat(ui.getLine());
 
         Trip newTrip = new Trip(index, name, startDate);
 
--- src/test/java/seedu/trippie/StorageTest.java
+++ src/test/java/seedu/trippie/StorageTest.java
@@ -14,7 +14,7 @@ import java.nio.file.Path;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Scanner;
 
 class StorageTest {
 
@@ -28,14 +28,14 @@ class StorageTest {
         FileWriter fw = new FileWriter(sampleMasterFile, false);
 
         fw.append(
-               "DEFAULT 6\n" +
-               "0,Chigago run,01-01-2021\n" +
-               "1,Jakarta Fest,01-02-2021\n" +
-               "2,Malaysia,20-08-2021\n" +
-               "3,Malaysia,20-02-2020\n" +
-               "4,Australia,20-12-2020\n" +
-               "5,America,20-11-2020\n" +
-               "6,Africa,04-03-2021\n"
+               "DEFAULT 6\n"
+               + "0,Chigago run,01-01-2021\n"
+               + "1,Jakarta Fest,01-02-2021\n"
+               + "2,Malaysia,20-08-2021\n"
+               + "3,Malaysia,20-02-2020\n"
+               + "4,Australia,20-12-2020\n"
+               + "5,America,20-11-2020\n"
+               + "6,Africa,04-03-2021\n"
         );
 
         fw.close();
@@ -52,13 +52,13 @@ class StorageTest {
         TrippieData data = new TrippieData(storage);
         storage.loadMasterFile(new Scanner(file), data);
 
-        String expectedTripListString = "0. 1 Day - Chigago run\n" +
-                "1. 1 Day - Jakarta Fest\n" +
-                "2. 1 Day - Malaysia\n" +
-                "3. 1 Day - Malaysia\n" +
-                "4. 1 Day - Australia\n" +
-                "5. 1 Day - America\n" +
-                "6. 1 Day - Africa\n";
+        String expectedTripListString = "0. 1 Day - Chigago run\n"
+                + "1. 1 Day - Jakarta Fest\n"
+                + "2. 1 Day - Malaysia\n"
+                + "3. 1 Day - Malaysia\n"
+                + "4. 1 Day - Australia\n"
+                + "5. 1 Day - America\n"
+                + "6. 1 Day - Africa\n";
 
         assertEquals(expectedTripListString, data.list());
     }
